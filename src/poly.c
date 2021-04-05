a3ac21b93e96c7a678d9beabb16e0bf4414c46e4
Paweł Fijałkowski <pawel.fijalkowski@sidnet.info>
2021-04-05 17:08:40 +0200

    Initial code structure

---
+++ src/poly.c
@@ -0,0 +1,54 @@
+#include "poly.h"
+
+void PolyDestroy(Poly *p) {
+
+}
+
+Poly PolyClone(const Poly *p) {
+    Poly result;
+    return result;
+}
+
+Poly PolyAdd(const Poly *p, const Poly *q) {
+    Poly result;
+    return result;
+}
+
+Poly PolyAddMonos(size_t count, const Mono *monos) {
+    Poly result;
+    return result;
+}
+
+Poly PolyMul(const Poly *p, const Poly *q) {
+    Poly result;
+    return result;
+}
+
+Poly PolyNeg(const Poly *p) {
+    Poly result;
+    return result;
+}
+
+Poly PolySub(const Poly *p, const Poly *q) {
+    Poly result;
+    return result;
+}
+
+poly_exp_t PolyDegBy(const Poly *p, size_t var_idx) {
+    return 0;
+}
+
+poly_exp_t PolyDeg(const Poly *p) {
+    return 0;
+}
+
+bool PolyIsEq(const Poly *p, const Poly *q) {
+    return 0;
+}
+
+Poly PolyAt(const Poly *p, poly_coeff_t x) {
+    Poly result;
+    return result;
+}
+
+
